e348f25909ba43ce95308f27f97ac971abc8463d
Win10 <dfdffdffd@>
2023-08-24 16:05:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/week6homework1andhomework2/OdevAciklamasi.kt
@@ -0,0 +1,21 @@
+package com.example.week6homework1andhomework2
+
+// 6. Hafta  1. ve 2. Ödevler:
+
+
+// Odev 1: Navigation Compenentla  "Ana Sayfa" fragmentı oluşturun ImageView ,
+// 2 EditText ve butona basıp diğer fragmenta geçiş yaptığında  başlık olarak "Hoş Geldiniz!" yazısını görmelisiniz.
+
+
+
+//  Odev  2:  Navigation Component kullanarak üç farklı fragmentı sırayla açın.
+//  Ardından geri tuşuna basıldığında Anasayfaya yönlendirsin.
+
+
+
+
+//Bu projede yukarıdaki 2 ödevi kapsamakla birlikte ek olarak kullandığım kodlar da bulunuyor. Bunlar:
+
+// Hoş geldiniz sayfasında Hoşgeldiniz yazısına ek olarak kullanıcının bir önceki sayfada girdiği verileri ekranda yazdıran (veri transferi) kodu,
+//  3. fragmenta geçtikten sonra burada da kullanıcıdan veri alıp
+//  YENİDEN hoşgeldiniz yazan sayfaya dönen bir action var . Bu sefer kullanıcının girdiği yeni verileri ekrana yazdırıyor.
---
+++ app/src/main/java/com/example/week6homework1andhomework2/User.kt
@@ -0,0 +1,6 @@
+package com.example.week6homework1andhomework2
+
+import java.io.Serializable
+
+data class User ( var name:String, var pswrd:String): Serializable {
+}
